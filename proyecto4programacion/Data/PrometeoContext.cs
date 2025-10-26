4293eac3bdc9d2447c8b9edf2d86c79d20540707
Agustin Blancat <agustinblancat@gmail.com>
2025-10-26 18:16:22 -0300

    agregado clase recordatorio y dbSet

--- proyecto4programacion/Data/PrometeoContext.cs
+++ proyecto4programacion/Data/PrometeoContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using proyecto4programacion.Models;
 
 namespace proyecto4programacion.Data
 {
     public class PrometeoContext : DbContext
     {
         public PrometeoContext(DbContextOptions<PrometeoContext> options) : base(options) { }
+
+        public DbSet<Recordatorio> recordatorios { get; set; }
     }
 }
---
+++ proyecto4programacion/Models/Recordatorio.cs
@@ -0,0 +1,19 @@
+﻿namespace proyecto4programacion.Models
+{
+    public class Recordatorio
+    {
+        public int Id { get; set; }
+
+        public string titulo { get; set; }
+
+        public string fechaRecordatorio { get; set; }
+
+        public string hora { get; set; }
+
+        public string duracion { get; set; }
+
+        public string descripcion { get; set; }
+
+        //Notificacion? Personas compartidas del recordatorio? importancia?
+    }
+}
