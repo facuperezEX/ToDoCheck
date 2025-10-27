141161a8e4078c3ef0ed6d5e16054000e76b50fe
facundo <00facuperez@gmail.com>
2025-10-27 15:09:56 -0300

    add atributo en entidad tarea

--- proyecto4programacion/Entities/Tarea.cs
+++ proyecto4programacion/Entities/Tarea.cs
@@ -6,8 +6,9 @@ namespace proyecto4programacion.Entities
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
-        public DateTime FechaCreacion { get; set; }
-        public string Estado { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public DateTime? FechaCompletado { get; set; }
+        public string? Estado { get; set; }
 
         public Tarea() { }
     }
---
+++ proyecto4programacion/Migrations/20251027180621_addFechaCompletado.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proyecto4programacion.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFechaCompletado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaCreacion",
+                table: "Tarea",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaCompletado",
+                table: "Tarea",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaCompletado",
+                table: "Tarea");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaCreacion",
+                table: "Tarea",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
